1b053a15237d6dad96d60b0e14e320919b2164f5
Darren S. Firth <dsfirth@gmail.com>
2026-03-27 20:06:16 -0400

    re-factor to support multiple output types; begin png-encoder work

--- maze.py
+++ maze.py
@@ -1,10 +1,14 @@
-from random import choice
 import click
 
-from src.grid import Grid
+# algorithms
+from src.algorithm.binary_tree import binary_tree
+from src.algorithm.sidewinder import sidewinder
 
-from src.binary_tree import binary_tree
-from src.sidewinder import sidewinder
+# encoders
+from src.encoder.png_encoder import png_encoder
+from src.encoder.text_encoder import text_encoder
+
+from src.grid import Grid
 
 
 @click.group()
@@ -13,9 +17,11 @@ def main():
 
 
 @main.command()
-@click.option('--size', '-s', nargs=2, default=(4, 4))
-@click.option('--algorithm', '--alg', '-a', type=click.Choice(['binary_tree', 'sidewinder'], case_sensitive=False), default='sidewinder')
-def grid(size, algorithm) -> None:
+@click.option('--size', '-s', nargs=2, default=(4, 4), help='Number of rows columns in the grid (default: 4 4)')
+@click.option('--algorithm', '--alg', '-a', type=click.Choice(['binary_tree', 'sidewinder'], case_sensitive=False), default='sidewinder',
+              help='Maze generation algorithm (default: sidewinder).')
+@click.option('--output', '--out', '-o', type=click.File('wb'), default='-')
+def grid(size, algorithm, output: click.File) -> None:
     rows, columns = size
     grid = Grid(rows, columns)
 
@@ -24,8 +30,15 @@ def grid(size, algorithm) -> None:
         'sidewinder': sidewinder
     }
 
-    algorithm_map.get(algorithm)(grid)
-    print(grid)
+    encoder_map = {
+        'png': png_encoder,
+        'text': text_encoder
+    }
+
+    encoder = 'png'
+
+    algorithm_map.get(algorithm.lower())(grid)
+    encoder_map.get(encoder.lower())(grid, output)
 
 
 if __name__ == '__main__':
---
+++ src/encoder/png_encoder.py
@@ -0,0 +1,36 @@
+import click
+import png
+
+from src.grid import Grid
+
+cell_size = 10
+
+def png_encoder(grid: Grid, output: click.File) -> None:
+    white_pixel = [255, 255, 255]
+    width, height = cell_size * grid.columns + 1, cell_size * grid.rows + 1
+
+    # initialize an empty canvas
+    rows = []
+    for y in range(height):
+        row = []
+        for x in range(width):
+            # Generate pixel values based on position
+            row.extend(white_pixel)
+        rows.append(row)
+
+
+    # # Create from array
+    # image_2d = [[255, 0, 0],    # Red pixel
+    #             [0, 255, 0],    # Green pixel
+    #             [0, 0, 255]]    # Blue pixel
+    # for row in grid.each_row():
+    #     # initialize pixel rows for the grid row
+    #     pixel_rows = [[]] * cell_size
+
+    #     for cell in row:
+    #         for y in range(cell_size):
+    #             for x in range(cell_size):
+
+
+    writer = png.Writer(width, height, bitdepth=8, greyscale=False)
+    writer.write(output, rows)
