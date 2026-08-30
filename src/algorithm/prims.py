56ddcf2f0be1a65616026edc3ea4e43e66e7b47d
Darren S. Firth <dsfirth@gmail.com>
2026-08-30 18:57:29 -0400

    .

--- src/algorithm/prims.py
+++ src/algorithm/prims.py
@@ -3,5 +3,15 @@ import random
 from src.grid import Grid
 
 
+def _add_frontier():
+    return
+
+def _mark():
+    return
+
+
 def prims(grid: Grid) -> Grid:
+    grid = [[0 for _ in range(grid.columns)] for _ in range(grid.rows)]
+    frontier = []
+
     return grid
