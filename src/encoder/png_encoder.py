f61483c769ccdcf9424d6c8e9df462c83ae8d2c5
Darren S. Firth <dsfirth@gmail.com>
2026-09-03 20:55:51 -0400

    add grid + margin to png_encoder output

--- src/encoder/png_encoder.py
+++ src/encoder/png_encoder.py
@@ -3,9 +3,13 @@ import png
 
 from src.grid import Grid
 
+# define some constants
 cell_size = 10
+margin = 5
+
 
 def png_encoder(grid: Grid, output: click.File) -> None:
+    black_pixel = [0, 0, 0]
     white_pixel = [255, 255, 255]
     width, height = cell_size * grid.columns + 1, cell_size * grid.rows + 1
 
@@ -15,8 +19,27 @@ def png_encoder(grid: Grid, output: click.File) -> None:
         row = []
         for x in range(width):
             # Generate pixel values based on position
-            row.extend(white_pixel)
+            if y % cell_size == 0 or x % cell_size == 0:
+                row.extend(black_pixel)
+            else:
+                row.extend(white_pixel)
+
         rows.append(row)
+
+    # add margin
+    for _ in range(margin):
+        row = white_pixel * width
+        rows.insert(0, row)  # top margin
+
+        row = white_pixel * width
+        rows.append(row)  # bottom margin
+
+    for row in rows:
+        row[0:0] = white_pixel * margin  # left margin
+        row.extend(white_pixel * margin)  # right margin
+
+    height += 2 * margin
+    width += 2 * margin
 
     # idea: use modulo (%) and int(x / cell_size) to get grid cell
 
@@ -31,7 +54,6 @@ def png_encoder(grid: Grid, output: click.File) -> None:
     #     for cell in row:
     #         for y in range(cell_size):
     #             for x in range(cell_size):
-
 
     writer = png.Writer(width, height, bitdepth=8, greyscale=False)
     writer.write(output, rows)
